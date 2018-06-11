50c9feece032fd7117129d0c68563fba4ac4770f
xiaolin <462441355@qq.com>
2018-06-11 17:06:59 +0800

    * 根据订单ID或者用户ID获取数据库名

--- app/Support/helper.php
+++ app/Support/helper.php
@@ -44,3 +44,26 @@ if (!function_exists('get_order_id')) {
         return \App\Support\Id\idClient::getInstance()->id($userId % 1024, rand(0, 4095));
     }
 }
+
+if (!function_exists('get_schema_id')) {
+    /**
+     * @desc   根据订单ID或者用户ID获取数据库名
+     * @author xl
+     * @param $id
+     * @param $userId
+     * @return mixed
+     */
+    function get_schema_id($id = null, $userId = null)
+    {
+        if (isset($id)) {
+            $num = str_pad(decbin($id), 64, '0', STR_PAD_LEFT);
+            $bit = substr($num, 42, 10); // userId 的10个bit位
+            $bit = substr($bit, -3, 3);
+            return bindec($bit);
+        }
+
+        $bit = decbin($userId);
+        $bit = substr($bit, -3, 3);
+        return bindec($bit);
+    }
+}
