8f05c93fce8deb6427806303dd00dc14421aa1d0
xiaolin <462441355@qq.com>
2018-06-11 14:17:00 +0800

    * 增加errorCode

--- app/Support/Enums/ErrorCode.php
+++ app/Support/Enums/ErrorCode.php
@@ -19,6 +19,21 @@ class ErrorCode extends Enum
     public static $ENUM_SYSTEM_ERROR = 400;
 
     /**
+     * @Message('CURL接口访问失败')
+     */
+    public static $ENUM_SYSTEM_CURL_ERROR = 401;
+
+    /**
+     * @Message('API 配置不存在')
+     */
+    public static $ENUM_SYSTEM_API_CONFIG_NOT_EXIST = 402;
+
+    /*
+     * @Message('API请求参数非法')
+     */
+    public static $ENUM_SYSTEM_API_REQUEST_ILLEGAL = 403;
+
+    /**
      * @Message('参数错误')
      */
     public static $ENUM_SYSTEM_API_PARAM_ERROR = 1000;
