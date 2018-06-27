4391a08a7c6b1f960ecf5bb75278ae21c7a79dbc
xiaolin <462441355@qq.com>
2018-06-27 13:33:37 +0800

    * 删除不必要的类

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -8,7 +8,6 @@ use App\Src\Basic\Filter;
 use App\Src\Form\User\InfoForm;
 use App\Src\Service\UserService;
 use App\Support\Enums\ErrorCode;
-use OkamiChen\TableShard\TableShardProvider;
 
 class UserController extends Controller
 {
